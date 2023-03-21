4d8c117becc49dc9718b2c6b592417d58a49a3a4
sam <sammuthuraj33@gmail.com>
2023-03-20 19:16:07 -0700

    beginer

---
+++ radious.py
@@ -0,0 +1,9 @@
+def area_of_circle(r):
+    a=3.14*r*r
+    return (a)
+        
+def volume_of_sphere(r):
+    vs=4/3*3.14*r*r*r
+    print(vs)
+    
+        
---
+++ radiousandvolumefind.py
@@ -0,0 +1,13 @@
+from radious import area_of_circle
+
+r=int(input("enter a number: "))
+print(area_of_circle(r))
+area_of_radious=area_of_circle(r)
+vs=4/3*area_of_radious*r
+print(vs)
+
+
+from radious import volume_of_sphere
+
+r=int(input("enter a number :"))
+volume_of_sphere(r)
