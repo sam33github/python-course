0af3e43c31aadd509d83265df81636ecd5ae414c
sam33github <sammuthuraj33@gmail.com>
2023-04-13 20:16:32 -0700

    sam1

---
+++ hello.py
@@ -0,0 +1,11 @@
+f=open("text.txt","r")
+
+
+# username="sam"
+# password="arun100jahanvinbegham"
+# line=username+":"+password
+# f.write(line)
+# f.close()
+for line in f:
+    print(line,end="")
+
